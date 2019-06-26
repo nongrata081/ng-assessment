cec4405263ec1af0327b9e05c1be5462d28690fb
Vahan Minasian <vaanatwork@gmail.com>
2019-06-26 17:35:18 +0200

    feat(i18n): add i81n with ngx-translate to shipping app
    feat #31

--- apps/shipping/src/app/app.module.ts
+++ apps/shipping/src/app/app.module.ts
@@ -1,9 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { UiComponentsModule } from '@ss/ui-components';
+import {
+	TranslateModule,
+	TranslateLoader
+} from '@ngx-translate/core';
+import {
+	HttpClientModule,
+	HttpClient
+} from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+	return new TranslateHttpLoader(http);
+}
 
 @NgModule({
 	declarations: [AppComponent],
@@ -15,7 +28,15 @@ import { UiComponentsModule } from '@ss/ui-components';
 			initialNavigation: 'enabled'
 		}),
 		RouterModule,
-		UiComponentsModule
+		UiComponentsModule,
+		HttpClientModule,
+		TranslateModule.forRoot({
+			loader: {
+				provide: TranslateLoader,
+				useFactory: HttpLoaderFactory,
+				deps: [HttpClient]
+			}
+		})
 	],
 	providers: [],
 	bootstrap: [AppComponent]
