4db19f09f9e414140d4bacc08d9732a155195dcf
Vahan Minasian <vaanatwork@gmail.com>
2019-06-26 18:54:30 +0200

    feat(unit-tests-hooks): run unit tests for apps & libs on pre-push
    feat #11

--- apps/shipping/src/app/app.component.spec.ts
+++ apps/shipping/src/app/app.component.spec.ts
@@ -1,13 +1,18 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { RouterTestingModule } from '@angular/router/testing';
-import { UiComponentsModule } from '@ss/ui-components';
+import {
+	TranslateModule,
+	TranslateService
+} from '@ngx-translate/core';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('AppComponent', () => {
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			imports: [RouterTestingModule, UiComponentsModule],
-			declarations: [AppComponent]
+			imports: [TranslateModule.forRoot()],
+			declarations: [AppComponent],
+			providers: [TranslateService],
+			schemas: [NO_ERRORS_SCHEMA]
 		}).compileComponents();
 	}));
 
