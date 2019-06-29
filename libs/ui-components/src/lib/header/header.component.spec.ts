5541c4d308c76d799805fcda6a581f634f0a1476
Vahan Minasian <vaanatwork@gmail.com>
2019-06-29 12:49:29 +0200

    feat(header-ui-component): add resuable ui component header
    feat #121

---
+++ libs/ui-components/src/lib/header/header.component.spec.ts
@@ -0,0 +1,26 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HeaderComponent } from './header.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+
+describe('HeaderComponent', () => {
+	let component: HeaderComponent;
+	let fixture: ComponentFixture<HeaderComponent>;
+
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			imports: [MDBBootstrapModule],
+			declarations: [HeaderComponent]
+		}).compileComponents();
+	}));
+
+	beforeEach(() => {
+		fixture = TestBed.createComponent(HeaderComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
+
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+});
--- libs/ui-components/src/lib/ui-components.module.ts
+++ libs/ui-components/src/lib/ui-components.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedUiComponentExampleComponent } from './shared-ui-component-example/shared-ui-component-example.component';
+import { HeaderComponent } from './header/header.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 @NgModule({
-	imports: [CommonModule],
-	declarations: [SharedUiComponentExampleComponent],
-	exports: [SharedUiComponentExampleComponent]
+	imports: [CommonModule, MDBBootstrapModule],
+	declarations: [HeaderComponent, SharedUiComponentExampleComponent],
+	exports: [HeaderComponent, SharedUiComponentExampleComponent]
 })
 export class UiComponentsModule {}
