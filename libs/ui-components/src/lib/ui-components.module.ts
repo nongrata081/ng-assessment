da38e3faba109c422e6cb9fcd76c8416513adaf9
Vahan Minasian <vaanatwork@gmail.com>
2019-06-29 14:05:52 +0200

    feat(footer): add reusable footer ui component
    feat #125

--- libs/ui-components/src/lib/ui-components.module.ts
+++ libs/ui-components/src/lib/ui-components.module.ts
@@ -3,10 +3,15 @@ import { CommonModule } from '@angular/common';
 import { SharedUiComponentExampleComponent } from './shared-ui-component-example/shared-ui-component-example.component';
 import { HeaderComponent } from './header/header.component';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
 	imports: [CommonModule, MDBBootstrapModule],
-	declarations: [HeaderComponent, SharedUiComponentExampleComponent],
-	exports: [HeaderComponent, SharedUiComponentExampleComponent]
+	declarations: [
+		HeaderComponent,
+		SharedUiComponentExampleComponent,
+		FooterComponent
+	],
+	exports: [HeaderComponent, SharedUiComponentExampleComponent, FooterComponent]
 })
 export class UiComponentsModule {}
